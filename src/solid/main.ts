6cdbf1ee9621ad78857a13eee587b92b702ba800
Pedro Victor <pedro20002victor@gmail.com>
2023-01-01 22:20:05 -0300

    test code functions

--- src/solid/main.ts
+++ src/solid/main.ts
@@ -2,6 +2,7 @@ import { ShoppingCart } from "./shopping";
 
 const spCart = new ShoppingCart();
 spCart.addItem({ name: "Caderno", price: 7 });
+spCart.addItem({ name: 'test', price: 9999});
 spCart.addItem({ name: "Lapis", price: 1.25 });
 spCart.addItem({ name: "Apontador", price: 3 });
 spCart.items;
